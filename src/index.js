da058075c2f174a2bd0597eb71597880d26ec22a
Zakeria Ali <zak.fined@gmail.com>
2019-04-17 22:40:47 +0100

    Initial File Structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/index.scss';
+import Routes from './Routes';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
