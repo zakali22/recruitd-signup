958eaa78d375230ceb44f8098e6b1642dbda5484
Zakeria Ali <zak.fined@gmail.com>
2019-04-18 15:18:32 +0100

    Design left side of Signup Component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,8 +5,12 @@ class Signup extends Component {
     return (
       <div className="signup">
         <div className="signup__left">
+          <img src="/images/top.png" alt="Top left image" id="imgLeft"/>
+          <img src="/images/logo.png" alt="Recruitd image" className="signup__left--logo"/>
+          <img src="/images/bottom.png" alt="Bottom right image" id="imgRight" />
         </div>
         <div className="signup__right">
+
         </div>
       </div>
     );
