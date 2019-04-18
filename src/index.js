ee7e66030b4114aae18de88f0adfc9c1577ad451
Zakeria Ali <zak.fined@gmail.com>
2019-04-18 20:00:11 +0100

    Setup Redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './style/index.scss';
 import Signup from "./components/Signup";
 
+// Setup Redux
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore, compose} from "redux";
+import promiseMiddleware from "redux-promise";
+import reduxThunk from "redux-thunk";
+import rootReducer from "./reducers";
 
-ReactDOM.render(<Signup />, document.getElementById('root'));
+
+const middlewares = [reduxThunk, promiseMiddleware];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
+const store = createStore(
+  rootReducer,
+  {},
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+ReactDOM.render(
+        <Provider store={store}>
+            <Signup />
+        </Provider>, 
+    document.getElementById('root')
+);
 
